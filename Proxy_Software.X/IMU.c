737c8002467b805a125fba02a5df2445f0cc9944
Kylie Angel <kyliea@vt.edu>
2022-08-29 21:41:57 -0400

    Updates

--- Proxy_Software.X/IMU.c
+++ Proxy_Software.X/IMU.c
@@ -8,13 +8,24 @@
 
 #include "xc.h"
 #include "IMU.h"
+#include "i2c1.h"
+
+#define I2C_ADDR_LSM6
 
 //TODO: Implement data retrieval IMU function
 uint8_t ReadIMU(void){
     
 // Variables for final converted Gyroscopic data
 volatile uint8_t GyrX, GryY, GryZ;
-    
+uint8_t LSM6_cmd = 0;
+I2C1_MESSAGE_STATUS status;
+
+I2C1_MasterWrite(LSM6_cmd, 1, I2C_ADDR_LSM6, &status);
+while (I2C1_MESSAGE_PENDING == status);
+
+I2C1_MasterRead( &, 1, I2C_ADDR_LSM6, &status);
+while (I2C1_MESSAGE_PENDING == status);
+
 }
 
 
